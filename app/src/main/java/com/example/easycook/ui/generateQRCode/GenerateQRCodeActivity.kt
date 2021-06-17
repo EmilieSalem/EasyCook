b12ee3d81744f62ea2d03c0983f1e05ed5c6e9ea
pclavel <clavel.pdj@gmail.com>
2021-06-17 14:07:36 +0200

    lien avce les recettes

--- app/src/main/java/com/example/easycook/ui/generateQRCode/GenerateQRCodeActivity.kt
+++ app/src/main/java/com/example/easycook/ui/generateQRCode/GenerateQRCodeActivity.kt
@@ -6,9 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import com.example.easycook.R
 import com.example.easycook.data.DataManager
+import com.google.gson.Gson
 import com.google.zxing.BarcodeFormat
 import com.journeyapps.barcodescanner.BarcodeEncoder
 
@@ -17,22 +19,21 @@ class GenerateQRCodeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_generate_qrcode)
 
-        /* Exemple : code pour récupérer la recette à partir de son id et l'afficher dans le Logcat"
+        val qrcodeImageView = findViewById<ImageView>(R.id.qrCodeImageView)
         val bundle = this.intent.extras
         val recipeId = bundle?.getString("recipeId") ?: "Pas de recipeId trouvé"
-        Log.i("TEST_ID", DataManager.getRecipeById(recipeId, this).toString())
-         */
 
-        val qrcodeImageView = findViewById<ImageView>(R.id.qrCodeImageView)
+        val recipe = DataManager.getRecipeById(recipeId, this)
+        val recipeJson = Gson().toJson(recipe)
 
-        val content = "IdRecette"
         val barcodeEncoder = BarcodeEncoder()
-        val bitmap = barcodeEncoder.encodeBitmap(content, BarcodeFormat.QR_CODE, 512, 512)
+        val bitmap = barcodeEncoder.encodeBitmap(recipeJson, BarcodeFormat.QR_CODE, 512, 512)
 
         qrcodeImageView.setImageBitmap(bitmap)
+
+        Toast.makeText(this@GenerateQRCodeActivity, recipeJson, Toast.LENGTH_LONG).show()
     }
 
-    // TODO : A modifier si besoin
     companion object {
         fun navigateToGenerateQRCode(context : Context, recipeId : String) {
             val toGenerateQRCode = Intent(context, GenerateQRCodeActivity::class.java)
--- app/src/main/java/com/example/easycook/ui/scanQRCode/ScanQRCodeActivity.kt
+++ app/src/main/java/com/example/easycook/ui/scanQRCode/ScanQRCodeActivity.kt
@@ -7,6 +7,10 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.example.easycook.R
+import com.example.easycook.data.DataManager
+import com.example.easycook.model.Recipe
+import com.example.easycook.ui.recipeList.RecipeListActivity
+import com.google.gson.Gson
 import com.google.zxing.integration.android.IntentIntegrator
 
 
@@ -16,8 +20,6 @@ class ScanQRCodeActivity : AppCompatActivity() {
         setContentView(R.layout.activity_scan_qrcode)
 
         val integrator = IntentIntegrator(this)
-        integrator.setPrompt("Scan a barcode or QR Code")
-        integrator.setOrientationLocked(false)
         integrator.initiateScan()
     }
 
@@ -27,9 +29,12 @@ class ScanQRCodeActivity : AppCompatActivity() {
             val value = result.contents
             if (value != null) {
                 Toast.makeText(this@ScanQRCodeActivity, value, Toast.LENGTH_SHORT).show()
-                // do something
+                val recipe = Gson().fromJson(value, Recipe::class.java)
+                DataManager.saveRecipe(recipe,this)
+                RecipeListActivity.navigateToRecipeList(this)
             } else {
                 Toast.makeText(this@ScanQRCodeActivity, "Cancelled", Toast.LENGTH_SHORT).show()
+                RecipeListActivity.navigateToRecipeList(this)
             }
         } else {
             super.onActivityResult(requestCode, resultCode, data)
